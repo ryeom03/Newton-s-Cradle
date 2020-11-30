e5f9fd57273cf3706e2052e069cfc4e1035789c0
ryeom03 <73701063+ryeom03@users.noreply.github.com>
2020-11-30 16:22:55 +0530

    Update roof.js

--- roof.js
+++ roof.js
@@ -12,7 +12,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("lightblue");
+      fill("black");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
